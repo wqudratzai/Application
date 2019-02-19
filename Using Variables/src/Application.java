f46d25d999da13313f03886b8a784bc09cfb68cc
wqudr <wqudr@DESKTOP-M6TJ923.hsd1.va.comcast.net>
2019-02-18 21:17:20 -0500

    Initial checkin of Application

---
+++ Using Variables/src/Application.java
@@ -0,0 +1,25 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int MyNumber = 88;
+		int MyName = 571;
+		short myshort = 874;
+		long mylong = 9797;
+		double mydouble = 7.3243;
+		float myfloat = 324.3f;
+		char mychar = 'a';
+		boolean myboolean = true;
+		byte mybyte = 127;
+System.out.println(MyNumber);
+System.out.println(myshort);
+System.out.println(mylong);
+System.out.println(mydouble);
+System.out.println(myfloat);
+System.out.println(mychar);
+System.out.println(myboolean);
+System.out.println(mybyte);
+System.out.println(MyName);
+	}
+
+}
