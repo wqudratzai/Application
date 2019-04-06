576b23ba5d8ad6c87a85ee3aa083781f55aca4f1
wqudr <wqudr@DESKTOP-M6TJ923.hsd1.va.comcast.net>
2019-04-06 16:03:55 -0400

    ATT-101 Created first automation

--- Using Variables/src/Application.java
+++ Using Variables/src/Application.java
@@ -20,9 +20,7 @@ System.out.println(mychar);
 System.out.println(myboolean);
 System.out.println(mybyte);
 System.out.println(MyName);
-System.out.println("Test 1");
-System.out.println("Test 2");
-System.out.println("Test 3");
+
 	}
 
 }
